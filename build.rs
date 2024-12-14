b99feedf883ae4af764a3ee4a9c442f9a85a1436
Alexander Evgin <aleasims@gmail.com>
2024-12-14 18:40:15 +0400

    Add gevulot-rs version to gvltctl --version output

--- build.rs
+++ build.rs
@@ -1,6 +1,5 @@
 fn main() -> shadow_rs::SdResult<()> {
     let mut deny = std::collections::BTreeSet::new();
     deny.insert(shadow_rs::CARGO_TREE);
-    deny.insert(shadow_rs::CARGO_METADATA);
     shadow_rs::new_deny(deny)
 }
